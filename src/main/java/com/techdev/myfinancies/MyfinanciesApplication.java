3fe0e04e8c8a0eb8a967264e044e9832dcf0f9e1
Isaac Rocha <isaac.ziroldo@gmail.com>
2020-06-09 18:42:34 -0300

    project - spring

---
+++ src/main/java/com/techdev/myfinancies/MyfinanciesApplication.java
@@ -0,0 +1,13 @@
+package com.techdev.myfinancies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfinanciesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfinanciesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techdev/myfinancies/MyfinanciesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techdev.myfinancies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfinanciesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
